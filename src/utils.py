5c9385bca262aff53961b67fc7af67d4667f7e4e
ncgordeev <ncgordeev@gmail.com>
2023-12-17 23:27:30 +0300

    created sorted data function

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import json
 
 
-def load_transactions_from_file(file_path):
+def load_transactions_from_file(file_path: str) -> list:
     """
     Load transactions from json-file
     :param file_path:
@@ -10,3 +10,14 @@ def load_transactions_from_file(file_path):
     with open(file_path, encoding="utf-8") as file:
         data = json.load(file)
         return data
+
+
+def sorted_transactions(transactions: list) -> list:
+    """
+    Sort transactions by date
+    :param transactions:
+    :return:
+    """
+    operations = [operation for operation in transactions if operation.get("state") == "EXECUTED"]
+    sorted_operations = sorted(operations, key=lambda x: x["date"], reverse=True)
+    return sorted_operations
