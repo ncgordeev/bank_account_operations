82dffcd2473b60d102551641a46639777f887a7e
ncgordeev <ncgordeev@gmail.com>
2023-12-21 22:13:04 +0300

    add new method for masking card and account numbers

--- src/main_class.py
+++ src/main_class.py
@@ -20,6 +20,20 @@ class Transaction:
         formatted_date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
         return formatted_date.strftime("%d.%m.%Y")
 
+    @staticmethod
+    def masking_number(card_or_account_number):
+        split_number = card_or_account_number.split()
+
+        if card_or_account_number.startswith("Счет"):
+            number_for_masking = split_number.pop()
+            masking_number = f"**{number_for_masking[-4:]}"
+            split_number.append(masking_number)
+        else:
+            number_for_masking = split_number.pop()
+            masking_number = f"{number_for_masking[:6] + ' ** **** ' + number_for_masking[-4:]}"
+            split_number.append(masking_number)
+        return " ".join(split_number)
+
     def __str__(self) -> str:
         return f"{self.date}, {self.description},\n" \
                f"{self.trans_from}, {self.trans_to}, {self.amount}, {self.currency})"
