ce6ee8b2f82d88c9048f851822cf26991bfdc32b
ncgordeev <ncgordeev@gmail.com>
2023-12-21 22:10:46 +0300

    add new method for formatting date

--- src/main_class.py
+++ src/main_class.py
@@ -15,6 +15,11 @@ class Transaction:
         self.amount = amount
         self.currency = currency
 
+    @staticmethod
+    def formatting_date(date) -> str:
+        formatted_date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+        return formatted_date.strftime("%d.%m.%Y")
+
     def __str__(self) -> str:
         return f"{self.date}, {self.description},\n" \
                f"{self.trans_from}, {self.trans_to}, {self.amount}, {self.currency})"
