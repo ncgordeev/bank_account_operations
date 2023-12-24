55273c6eb0aadff86475e6c67fe258e70bf864c0
ncgordeev <ncgordeev@gmail.com>
2023-12-24 18:12:09 +0300

    tests complited

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 OPERATIONS_PATH = Path.joinpath(ROOT_PATH, "files", "operations.json")
 TEST_FILE_PATH = Path.joinpath(ROOT_PATH, "tests", "test_trans.json")
+TEST_EMPTY_JSON = Path.joinpath(ROOT_PATH, "tests", "test_empty_file.json")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,29 @@
+import json
+import random
+
 import pytest
 from src.main_class import Transaction
-from config import TEST_FILE_PATH
-from src.utils import load_transactions_from_file, sorted_transactions
+from config import OPERATIONS_PATH, TEST_FILE_PATH, TEST_EMPTY_JSON
+from src.utils import load_transactions_from_file, sorted_transactions, get_class_instances
 
 
 def test_load_transactions_from_file():
     operations = load_transactions_from_file(TEST_FILE_PATH)
     assert operations == [{"1": 1, "2": 2, "3": 3}]
+
+
+def test_load_empty_file():
+    with pytest.raises(json.JSONDecodeError):
+        load_transactions_from_file(TEST_EMPTY_JSON)
+
+
+def test_load_file_not_exist():
+    with pytest.raises(FileNotFoundError):
+        load_transactions_from_file("test.json")
+
+
+def test_get_class_instances():
+    operations = load_transactions_from_file(OPERATIONS_PATH)
+    sorted_operations = sorted_transactions(operations)
+    operations = get_class_instances(sorted_operations)
+    assert isinstance(operations[random.randint(0, 5)], Transaction)
